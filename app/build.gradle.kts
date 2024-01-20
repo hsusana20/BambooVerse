bf5aa0b54147d0fbed2dbebc0e0ecbc68f3824be
Heiro Usana <hsusana@student.apc.edu.ph>
2024-01-20 10:22:11 +0800

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
+
 
 
     implementation("com.google.mlkit:barcode-scanning:17.0.2")
